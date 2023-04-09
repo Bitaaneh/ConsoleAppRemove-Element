99ba870ad5baf3d58c7e2ee3a3abee12d7a47c9e
Bita Rahimkhani <taaghche.bitarahimkhani@gmail.com>
2023-04-10 00:45:52 +0430

    .20.01

--- ConsoleAppRemove Element/Program.cs
+++ ConsoleAppRemove Element/Program.cs
@@ -1,37 +1,25 @@
-﻿//var tmp = new int[8] { 0, 1, 2, 2, 3, 0, 4, 2 };
+﻿var tmp = new int[8] { 0, 1, 2, 2, 3, 0, 4, 2 };
 //var tmp = new int[4] { 3,2,2,3};
 //var tmp = new int[1] { 1};
-var tmp = new int[2] { 4,1};
-var res = RemoveElement(tmp, 4);
+//var tmp = new int[2] { 4,4};
+var res = RemoveElement(tmp, 2);
  int RemoveElement(int[] nums, int val)
 {
-    int counter = 0;
-    int valCounter = 0;
-    for(int i=0;i<nums.Length;i++)
+    int i = 0;
+    int n = nums.Length;
+    while (i < n)
     {
-        if (nums[i]==val)
+        if (nums[i] == val)
         {
-            counter = i;
-            valCounter++;
-            while ( counter+1 <nums.Length && counter < nums.Length)
-            {
-                if (nums[counter + 1] != val)
-                {
-                    nums[i] = nums[counter+1];
-                    i++;
-                    counter++;
-
-                }
-                else if (counter + 1 !=nums.Length && nums[counter + 1] == val)
-                {
-                    counter++;
-                    valCounter++;
-                }
-            }
-              
-            }
-    
+            nums[i] = nums[n - 1];
+            n--;
         }
+        else
+        {
+            i++;
+        }
+    }
+    return n;
+
     
-    return nums.Length - valCounter;
  }
